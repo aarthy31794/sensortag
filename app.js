23d3c6f1f65a0d250581a8f9811b5f6e27b121df
mallikarjuna  j <mallikarjuna.j@vensi.com>
2017-04-18 00:19:13 +0530

    Update
    Disabled the mouse right click event & text selection in html pages

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ app.controller('mainController', function ($scope, $mdToast, $mdDialog, sensorta
     $scope.timeDispData2 = [];
     $scope.timeDispData3 = [];
 
+    // Disabling the mouse right click event
+    document.addEventListener('contextmenu', event => event.preventDefault());
+
     // ---------- Line Graph Code START -----------
     $scope.lineGraphoptions = {
         chart: {
